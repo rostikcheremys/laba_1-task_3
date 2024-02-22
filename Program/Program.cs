56d8d73b89ce619e94b67ac2bb6b185593072d17
rostikcheremys <rostikcheremys@gmail.com>
2024-02-22 19:42:16 +0200

    fix problems

--- Program/Program.cs
+++ Program/Program.cs
@@ -8,14 +8,16 @@ namespace Program
         
         public static void Main()
         {
+            double precision = 0.0001;
+                
             Sum sum1 = n => 1.0 / Math.Pow(2, n);
-            Console.WriteLine($"Результат обчислення ряду 1 + 1/2 + 1/4 + 1/8 + 1/16 + ... : {Calculate(sum1, 0.00001)}");
+            Console.WriteLine($"Результат обчислення ряду 1 + 1/2 + 1/4 + 1/8 + 1/16 + ... : {Calculate(sum1, precision)}");
 
-            Sum sum2 = n => 1.0 / Factorial(n);
-            Console.WriteLine($"Результат обчислення ряду 1 + 1/2! + 1/3! + 1/4! + 1/5! + ... : {Calculate(sum2, 0.00001) - 1}");
+            Sum sum2 = n => 1.0 / Factorial(n + 1);
+            Console.WriteLine($"Результат обчислення ряду 1 + 1/2! + 1/3! + 1/4! + 1/5! + ... : {Calculate(sum2, precision)}");
             
             Sum sum3 = n => Math.Pow(-1, n) * 1.0 / Math.Pow(2, n);
-            Console.WriteLine($"Результат обчислення ряду -1 + 1/2 - 1/4 + 1/8 - 1/16 + ... : -{Calculate(sum3, 0.00001)}");
+            Console.WriteLine($"Результат обчислення ряду -1 + 1/2 - 1/4 + 1/8 - 1/16 + ... : -{Calculate(sum3, precision)}");
         }
         
         private static double Calculate(Sum data, double precision)
